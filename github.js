a9fb44aee715bcc168b7a331e1618ad433f073ba
Jerehme Gayle <jerehme.gayle@gmail.com>
2018-03-21 07:44:35 -0700

    connected to githug user repo API and now show users repos in UI

--- github.js
+++ github.js
@@ -2,15 +2,26 @@ class GitHub {
   constructor() {
     this.client_id = '723c8eb675e9c7cfdfe9';
     this.client_secret = '2ce190072b03470a00395797f42fd0222efd08fc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
+    // Fetching user profile data
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+    // Fetching user repos data
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    // Profile data response
     const profile = await profileResponse.json();
+    // Repo data response
+    const repos = await repoResponse.json();
+
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
